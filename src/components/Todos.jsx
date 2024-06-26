c1d403e4368e3f5a0692c554ea73d5ec4a2636ce
indira <indirayulianti67@gmail.com>
2024-06-26 22:04:26 +0700

    Develop toggle todo feature

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import TodoItem from './TodoItem'
 
-const Todos = ({ todos }) => {
-  return (
-    <div style={styles.container}>
-      {todos.map((todo) => {
-        return <TodoItem key={todo.id} todo={todo} />
-      })}
-    </div>
+const Todos = ({todos, toggleCompleted}) => {
+  return(
+      <div style={styles.container}>
+          {todos.map((todo) => {
+              return (
+                  <TodoItem 
+                      key={todos.id} 
+                      todo={todo} 
+                      toggleCompleted={toggleCompleted}    
+                  />
+              )
+          })}
+      </div>
   )
 }
 
